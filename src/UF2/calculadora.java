3ae953ecb2f61f0633c95f1d3e387037f7ad55ea
Ivan <belzebuth-88@hotmail.com>
2014-11-04 18:10:30 +0100

    Primer commit

---
+++ src/UF2/calculadora.java
@@ -0,0 +1,11 @@
+package UF2;
+
+public class calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sóc una calculadora simple");
+
+	}
+
+}
